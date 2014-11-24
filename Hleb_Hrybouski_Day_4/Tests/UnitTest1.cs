47d7505cf2666358dfd489910db56eb699ca6568
HlebHrybouski <lightellfir@gmail.com>
2014-11-24 12:38:27 +0300

    Task 4 of C# training

---
+++ Hleb_Hrybouski_Day_4/Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Task_1;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+
+
+        [TestCase()]
+        public void TestMethod1()
+        {
+        }
+    }
+}
